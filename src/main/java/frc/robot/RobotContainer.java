d014c1d653062781d63cee41f8df8d25774a0542
Glutinfree <111544394+Glutinfree@users.noreply.github.com>
2023-01-21 11:46:02 -0800

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.PistonSet;
-import static frc.robot.commands.PistonSet.SETCOMMAND;
+import static frc.robot.commands.PistonSet.SETCOMMAND; 
 import frc.robot.subsystems.Piston;
 
 /**
